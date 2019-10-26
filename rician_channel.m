4a1d38da4b52e56472f01c6360cbbe712d32f109
unknown <suhaspriyatham@gmail.com>
2019-10-26 17:06:01 -0500

    Need to perform sliding window for phase estimation at the demodulation. Everything else works without error

--- rician_channel.m
+++ rician_channel.m
@@ -1,42 +1,46 @@
+function g_t = rician_channel() 
+
+    N=70;
+    M=1/2*(N/2-1);
+    l=544;  %number of symbols
+
+    n = 1:M;
+    Ts = (0.5*10^-3)/l;
+    t0 = 0;
+    t=(t0+n(1)*Ts):Ts:(t0+n(length(n))*Ts);
 
 
-N=70;
-M=1/2*(N/2-1);
-l=544;  %number of symbols
+    fd=0;%20;      %doppler frequency
+    omega_d = 2*pi*fd;
+    omega_n = omega_d*cos(2*pi*n/N);
+    alpha = 0;
+    beta_n = n*pi/M;
 
-n = 1:M;
-Ts = (0.5*10^-3)/l;
-t0 = 0;
-t=(t0+n(1)*Ts):Ts:(t0+n(length(n))*Ts);
+    %Jakes's Model
+    for i=1:length(t)
+        gI_t(i) = sum(2*cos(beta_n).*cos(omega_n)*t(i))+sqrt(2)*cos(omega_d*t(i))*cos(alpha);
+        gQ_t(i) = sum(2*sin(beta_n).*cos(omega_n)*t(i))+sqrt(2)*cos(omega_d*t(i))*sin(alpha);
+    end
 
 
-fd=20;
-omega_d = 2*pi*fd;
-omega_n = omega_d*cos(2*pi*n/N);
-alpha = 0;
-beta_n = n*pi/M;
+    %Rayleigh distribution
+    gI_t = gI_t/sqrt(2*(M+1));
+    gQ_t = gQ_t/sqrt(2*(M));
 
-%Jakes's Model
-for i=1:length(t)
-    gI_t(i) = sum(2*cos(beta_n).*cos(omega_n)*t(i))+sqrt(2)*cos(omega_d*t(i))*cos(alpha);
-    gQ_t(i) = sum(2*sin(beta_n).*cos(omega_n)*t(i))+sqrt(2)*cos(omega_d*t(i))*sin(alpha);
+    g_t_Rayleigh = gI_t+1i*gQ_t;
+
+
+
+    % Rician fading
+    K = 200;        %fading factor in dB
+    A = 10^(-K/20);
+    gain = 1/(sqrt(1+10^(K/10)));
+
+    g_t_Rician = (1+g_t_Rayleigh*A)*gain;
+
+    g_t = g_t_Rician;
+
 end
-
-
-%Rayleigh distribution
-gI_t = gI_t/sqrt(2*(M+1));
-gQ_t = gQ_t/sqrt(2*(M));
-
-g_t_Rayleigh = gI_t+1i*gQ_t;
-
-
-
-% Rician fading
-K = 200;        %fading factor in dB
-A = 10^(-K/20);
-gain = 1/(sqrt(1+10^(K/10)));
-
-g_t_Rician = (1+g_t_Rayleigh*10^(-K/20))/sqrt(1+10^(-K/10));
 
 
 
--- wireless_channel_start.m
+++ wireless_channel_start.m
@@ -3,13 +3,18 @@ clc;
 close all;
 
 % inputs: 
-% nInfoBits, nUniqueWordBits, nGaurdBits, upSampler 
+% nInfoBits, nUniqueWordBits, nGaurdBits, upSampler, Lw, Ls
 % 
-% Variables:
+% Parameters:
 % nInfoBits = number of bits of information
 % nUniqueWordBits = number of bits in unique word
 % nGaurdBits = number of gaurd bits
 % 
+%Lw = Length of the window
+%Ls = Length of the step
+%
+%
+%
 % burst = 
 % burstSymbols
 % QPSK modulation
@@ -26,13 +31,13 @@ nUniquewordBits = 96;
 nGuardBits = 0;
 
 infoBits = randi(0:1, 1, nInfoBits);
-uniqueWord = randi(0:1, 1, nUniquewordBits);
+uniqueWord = kron([ones(1,12)], [0 0 0 1 1 1 1 0]);%randi(0:1, 1, nUniquewordBits);
 guardBits = randi(0:1, nGuardBits);
 
 
 %Burst Builder
-burst=[guardBits uniqueWord infoBits guardBits];
-
+%burst=[guardBits uniqueWord infoBits guardBits];
+burst= uniqueWord;
 
 % Modulation
 n = 4;      % n-PSK modulation
@@ -78,11 +83,11 @@ upSampler = 16;
 upSamplingMatrix = [1 zeros(1,upSampler-1)];
 upSampledSignal = kron(modulatedSignal, upSamplingMatrix);
 %debug
-x=upSampledSignal;
-figure
-plot(1:length(x),real(x));
-hold on;
-plot(1:length(x),imag(x));
+% x=upSampledSignal;
+% figure
+% plot(1:length(x),real(x));
+% hold on;
+% plot(1:length(x),imag(x));
 %
 
 % raised cosine filter and interpolation
@@ -91,27 +96,99 @@ interpolatedSignal = conv(NUM, upSampledSignal);
 % resizing the interpolatedSignal. remove extra bits 
 interpolatedSignal = interpolatedSignal(.5*(length(NUM)-1)+1:length(interpolatedSignal)-.5*(length(NUM)-1));
 
+s = interpolatedSignal;
 %debug
-length(interpolatedSignal)
-x=interpolatedSignal;
-figure
-plot(1:length(x),real(x));
-hold on;
-plot(1:length(x),imag(x));
+% length(interpolatedSignal)
+% x=interpolatedSignal;
+% figure
+% plot(1:length(x),real(x));
+% hold on;
+% plot(1:length(x),imag(x));
 %
-figure;
-histogram(real(x));
-figure;
-histogram(imag(x));
-figure;
-histogram(abs(real(x)));
+% figure;
+% histogram(real(x));
+% figure;
+% histogram(imag(x));
+% figure;
+% histogram(abs(real(x)));
 %
 
 
-%Channel Design
+%channel g_t 
+g_t=rician_channel();
+%noise
+noise = 0;%randn(1,length(interpolatedSignal));
+
+receivedSignal = conv(g_t,interpolatedSignal);
+% resizing the receivedSignal. remove extra bits and adding noise
+receivedSignal = receivedSignal(.5*(length(g_t)-1)+1:length(receivedSignal)-.5*(length(g_t)-1));
+receivedSignal = receivedSignal+noise;
+
+y=receivedSignal;
+% Down sampling by 16;
+ y=decimate(y,16);
+% s=decimate(s,16);
 
 
+uw=modulatedSignal(1,1:48);
+%uw=s;
 
-%channel g_t
+
+% Demodulation consits of three parts
+% Only for QPSK
+
+% 1. Channel Estimation
+Lw = 22;
+Ls = 10;
+k=0:7;
+for j=1:1
+    theta_uw=0;
+    phs=0;
+    yyy=0;
+    for l=1:Lw
+        phs = phs+phase(y(l)/uw(l));
+        yyy = yyy+y(l)^4;
+    end
+    phs=phs/Lw;
+    theta_k = 1/4*atan2(imag(yyy),real(yyy))+k*pi/4;
+end
+
+diff=2*pi;
+for j=0:7
+    if theta_k(j+1)>pi
+        theta_k(j+1)=theta_k(j+1)-2*pi;
+    elseif theta_k(j+1)<-pi
+        theta_k(j+1)=theta_k(j+1)+2*pi;
+    end
+    abs(theta_k(j+1)-phs)
+    if diff > abs(theta_k(j+1)-phs);
+        diff=abs(theta_k(j+1)-phs);
+        theta_hat = theta_k(j+1)
+   end
+end
+theta_k*180/pi
+theta_hat*180/pi
+phs*180/pi
+
+compensated = y(1:Lw+1)*(cos(theta_hat)-1i*sin(theta_hat));
+
+% Hard Slicer for symbol detection
+for j=1:Lw+1
+    symbolPhase=phase(compensated(j));
+    if symbolPhase <= pi/4 && symbolPhase >= -pi/4
+        b = [0 0];
+    elseif symbolPhase <= 3*pi/4 && symbolPhase >= pi/4
+        b = [0 1];
+    elseif symbolPhase <= -3*pi/4 || symbolPhase >= 3*pi/4
+        b = [1 1];
+    elseif symbolPhase <= -pi/4 && symbolPhase >= -3*pi/4
+        b = [1 0];
+    else
+        display("Error in Hard Slicer. check if statements correctly for angles appropriately");
+    end
+    bits((2*(j-1)+1):2*(j-1)+2) = b;
+end
 
 
+corrects = bits(1:2*(Lw+1))==burst(1:2*(Lw+1));
+c=sum(corrects)
